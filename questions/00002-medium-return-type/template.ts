623facc159e9bbfce48f17984629504ecf1806d7
Thomas Duong <9430600+cuongdtt@users.noreply.github.com>
2024-12-02 22:42:55 +0700

    00002: Get Return Type

--- questions/00002-medium-return-type/template.ts
+++ questions/00002-medium-return-type/template.ts
@@ -1,1 +1,5 @@
-type MyReturnType<T> = any
+// my
+type MyReturnType<T> = T extends (...arg: any) => infer R ? R : never
+
+// recommended
+type _MyReturnType<T extends Function> = T extends (...arg: any) => infer R ? R : never
