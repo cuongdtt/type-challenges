bb9d9369c2e1a4ca35314cdaa158a2b8fac4291b
Thomas Duong <9430600+cuongdtt@users.noreply.github.com>
2024-12-04 21:55:37 +0700

    14: first of array

--- questions/00014-easy-first/template.ts
+++ questions/00014-easy-first/template.ts
@@ -1,1 +1,7 @@
-type First<T extends any[]> = any
+// type First<T extends any[]> =
+//     T[0] extends {} ? T[0] : never
+
+// solution
+// type First<T extends any[]> = T extends [] ? never : T[0]
+
+type First<T extends any[]> = T extends [infer First, ...any] ? First : never
