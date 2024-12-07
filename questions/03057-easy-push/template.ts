5e858de76b9a534550e6a4229b7d794a3257ff7f
Thomas Duong <9430600+cuongdtt@users.noreply.github.com>
2024-12-07 23:06:09 +0700

    3057 push

--- questions/03057-easy-push/template.ts
+++ questions/03057-easy-push/template.ts
@@ -1,1 +1,7 @@
-type Push<T, U> = any
+type Push<T extends any[], U> = [...T, U]
+
+// solution
+// type Push<T extends unknown[], U> = [...T, U]
+
+// The unknown type represents any value. This is similar to the any type,
+// but is safer because it’s not legal to do anything with an unknown value:
