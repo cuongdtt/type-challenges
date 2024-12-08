855b0b38af8eca1ea7533eee0f8faad846695bf9
Thomas Duong <9430600+cuongdtt@users.noreply.github.com>
2024-12-08 13:25:17 +0700

    533 concat

--- questions/00533-easy-concat/template.ts
+++ questions/00533-easy-concat/template.ts
@@ -1,1 +1,6 @@
-type Concat<T, U> = any
+type Concat<T extends unknown[] | readonly unknown[], U extends unknown[] | readonly unknown[]>
+    = [...T, ...U]
+
+// Solution
+// type Tuple = readonly unknown[]
+// type Concat<T extends Tuple, U extends Tuple> = [...T, ...U]
