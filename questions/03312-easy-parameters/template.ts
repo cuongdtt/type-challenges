02fb84a2f29b0aeefd45f35befdb4c1551f484f9
Thomas Duong <9430600+cuongdtt@users.noreply.github.com>
2024-12-14 22:13:19 +0700

    3312 Parameters

--- questions/03312-easy-parameters/template.ts
+++ questions/03312-easy-parameters/template.ts
@@ -1,1 +1,5 @@
-type MyParameters<T extends (...args: any[]) => any> = any
+type MyParameters<T extends (...args: any[]) => any> =
+    T extends (...args: infer Params) => any ? Params : []
+
+// Solution
+// type MyParameters<T extends (...args: any[]) => any> = T extends (...any: infer S) => any ? S : any
