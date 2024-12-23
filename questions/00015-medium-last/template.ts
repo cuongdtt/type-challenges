4ff0649e4b2d6f4a2f108e9241662d119b3f7771
Thomas Duong <9430600+cuongdtt@users.noreply.github.com>
2024-12-23 21:56:10 +0700

    15 Last of Array

--- questions/00015-medium-last/template.ts
+++ questions/00015-medium-last/template.ts
@@ -1,1 +1,5 @@
-type Last<T extends any[]> = any
+type Last<T extends any[]> = T extends [...Array<any>, infer P] ? P : never
+
+// clever solution
+// type Last<T extends any[]> = [any, ...T][T["length"]];
+// add 1 more any item and get the last by length
