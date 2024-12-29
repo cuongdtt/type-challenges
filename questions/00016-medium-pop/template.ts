648ef8d2c4f496e0046da6e71ff9b998bd48d5f4
Thomas Duong <9430600+cuongdtt@users.noreply.github.com>
2024-12-29 13:44:39 +0700

    16 Pop

--- questions/00016-medium-pop/template.ts
+++ questions/00016-medium-pop/template.ts
@@ -1,1 +1,13 @@
-type Pop<T extends any[]> = any
+type Pop<T extends any[]> = T extends [...infer R, any] ? R : []
+
+// solution
+// type Pop<T extends any[]> = T extends [...infer R, infer _] ? R : never
+
+// solution 2
+// type Pop<T extends any[]> = T extends []
+//   ? []
+//   : T extends [...infer R, infer L]
+//   ? R
+//   : never;
+
+// type Pop<T extends any[]> = T extends [...infer R, any] ? R : T
