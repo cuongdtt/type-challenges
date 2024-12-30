97e4ddadd967004887a6c35e02e15fb40c0c0b9a
Thomas Duong <9430600+cuongdtt@users.noreply.github.com>
2024-12-30 23:08:43 +0700

    2595 PickByType

--- questions/02595-medium-pickbytype/template.ts
+++ questions/02595-medium-pickbytype/template.ts
@@ -1,1 +1,6 @@
-type PickByType<T, U> = any
+type PickByType<T, U> = {
+  [P in keyof T as T[P] extends U ? P : never]: U
+}
+
+// solution is the same: U vs T[P]
+// type PickByType<T, U> = { [P in keyof T as T[P] extends U ? P : never]: T[P] }
