42b71cffa6f3505e95e1b06b18299af2fd44be78
Thomas Duong <9430600+cuongdtt@users.noreply.github.com>
2024-12-31 17:17:39 +0700

    3326 BEM style string

--- questions/03326-medium-bem-style-string/template.ts
+++ questions/03326-medium-bem-style-string/template.ts
@@ -1,1 +1,5 @@
-type BEM<B extends string, E extends string[], M extends string[]> = any
+type BEM<B extends string, E extends string[], M extends string[]> =
+    `${B}${E[0] extends string ? `__${E[number]}` : ''}${M[0] extends string ? `--${M[number]}` : ''}`
+
+// solution 2
+type BEM2<B extends string, E extends string[], M extends string[]> = `${B}${E extends [] ? '' : `__${E[number]}`}${M extends [] ? '' : `--${M[number]}`}`
