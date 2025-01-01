2f724be0a5f5181f08dbc1ff43891e181db7a2bd
Thomas Duong <9430600+cuongdtt@users.noreply.github.com>
2025-01-01 22:12:15 +0700

    3062 Shift

--- questions/03062-medium-shift/template.ts
+++ questions/03062-medium-shift/template.ts
@@ -1,1 +1,6 @@
-type Shift<T> = any
+type Shift<T extends unknown[]> = T extends []
+  ? []
+  : T extends [any, ...infer R] ? R : never
+
+// another solution: return T in that case T is []
+type Shift2<T extends any[]> = T extends [any, ...infer R] ? [...R] : T
