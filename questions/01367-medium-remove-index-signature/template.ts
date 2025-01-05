5b8dd35ffd09eb37c1e99b86242a44c632300c14
Thomas Duong <9430600+cuongdtt@users.noreply.github.com>
2025-01-05 14:15:11 +0700

    1367 Remove Index Signature

--- questions/01367-medium-remove-index-signature/template.ts
+++ questions/01367-medium-remove-index-signature/template.ts
@@ -1,1 +1,12 @@
-type RemoveIndexSignature<T> = any
+// Solution
+// Why P = PropertyKey ? generic with distribute type
+
+type RemoveIndexSignature<T, P = PropertyKey> = {
+  [K in keyof T as P extends K
+    ? never
+    : K extends P
+      ? K
+      : never]: T[K]
+}
+
+// [key: string]: any: index signature
