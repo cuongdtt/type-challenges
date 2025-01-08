51ac9c66522e615ff014bb114bd614e8b5c1a870
Thomas Duong <9430600+cuongdtt@users.noreply.github.com>
2025-01-08 21:35:39 +0700

    2946 ObjectEntries

--- questions/02946-medium-objectentries/template.ts
+++ questions/02946-medium-objectentries/template.ts
@@ -1,1 +1,10 @@
-type ObjectEntries<T> = any
+// solution 1
+// type RemoveUndefined<T> = [T] extends [undefined] ? T : Exclude<T, undefined>
+
+// type ObjectEntries<T> = {
+//   [K in keyof T]-?: [K, RemoveUndefined<T[K]>]
+// }[keyof T]
+
+// solution 2
+type ObjectEntries<T extends object, K extends keyof T = keyof T>
+ = K extends K ? [K, T[K] extends undefined ? undefined : Required<T>[K]] : never
