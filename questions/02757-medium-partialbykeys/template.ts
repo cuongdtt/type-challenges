48a15f31d8b947e9c826d05c1fd14fb0d26b03df
Thomas Duong <9430600+cuongdtt@users.noreply.github.com>
2025-01-08 20:50:52 +0700

    2757 PartialByKeys

--- questions/02757-medium-partialbykeys/template.ts
+++ questions/02757-medium-partialbykeys/template.ts
@@ -1,1 +1,7 @@
-type PartialByKeys<T, K> = any
+type PartialByKeys1<T, K extends keyof T = keyof T> = {
+  [key in K]?: T[key]
+} & {
+  [P in keyof T as P extends K ? never : P]: T[P]
+}
+
+type PartialByKeys<T, K extends keyof T = keyof T> = Omit<PartialByKeys1<T, K>, never>
