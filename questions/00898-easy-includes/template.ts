784adf5dfccb7ce6b40c6972f131882ef04a8a33
Thomas Duong <9430600+cuongdtt@users.noreply.github.com>
2025-01-15 21:41:33 +0700

    898 Includes

--- questions/00898-easy-includes/template.ts
+++ questions/00898-easy-includes/template.ts
@@ -1,1 +1,20 @@
-type Includes<T extends readonly any[], U> = any
+type IsEqual<T, U> =
+	(<G>() => G extends T ? 1 : 2) extends
+	(<G>() => G extends U ? 1 : 2)
+	  ? true
+	  : false
+
+type Includes<T extends readonly any[], U> =
+    T extends [infer F, ...infer Rest] ? IsEqual<F, U> extends true ? true : Includes<Rest, U> : false
+
+// solution
+// type Includes<T extends readonly any[], U> = {
+//   [P in T[number]]: true
+// }[U] extends true ? true : false
+
+// recursion
+// type Includes<T extends readonly any[], U> =
+//   T['length'] extends 0 ? false : // recursion terminal case
+//     T extends [infer First, ...infer Rest] ? // recursion decomposition
+//       Equal<First, U> extends true ? // recursive test
+//         true : Includes<Rest, U> : never // actual recursion
